4719c4d22b9c542c5812503ca03618098d22cef9
profdrac <ashu4hire@gmail.com>
2021-10-07 19:32:37 +0530

    Add files via upload

---
+++ 8. Iterative Constructs in Java/natural100_1.java
@@ -0,0 +1,14 @@
+package class9.chapter8;
+
+//WAP to print first 100 natural numbers in reverse
+
+class natural100_1
+{
+    public static void main()
+    {
+        for(int i=100; i>=1; i--)
+        {
+            System.out.println(i);
+        }
+    }
+}
---
+++ 8. Iterative Constructs in Java/natural1_100.java
@@ -0,0 +1,14 @@
+package class9.chapter8;
+
+//WAP to print first 100 natural numbers
+
+class natural1_100
+{
+    public static void main()
+    {
+        for(int i=1; i<=100; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
